760f385aa1ba765ecce7ec628b70e7075c13898d
felipesaliba5-design <felipesaliba5@gmail.com>
2026-07-14 10:31:56 -0300

    criação da classe conversor e dos metodos de conversão

--- Exercício de Fixação 02/projeto/conversores.js
+++ Exercício de Fixação 02/projeto/conversores.js
@@ -2,4 +2,20 @@
 
 class Conversor {
 
+   celsiusParaFahrenheit(celsius) {
+        let fahrenheit = (9 / 5) * celsius + 32;
+        return fahrenheit;
+      }
+
+    quilometrosParaMilhas(quilometros){
+        let Milhas = quilometros * 0.621371;
+        return Milhas;  
+      }
+
+    minutosParaHoras(minutos){
+        let minutos = horas / 60; 
+        return minutos;
+    }  
 }
+
+export { Conversor };
