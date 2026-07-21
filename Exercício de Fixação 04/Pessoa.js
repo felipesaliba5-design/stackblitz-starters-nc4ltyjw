bef9d94cf86eec89a1cb4fb104d599ee66510041
felipesaliba5-design <felipesaliba5@gmail.com>
2026-07-21 12:32:46 -0300

    Cria classe Pessoa com encapsulamento

--- Exercício de Fixação 04/Pessoa.js
+++ Exercício de Fixação 04/Pessoa.js
@@ -3,9 +3,28 @@ class Pessoa{
     #email;
 
     setNome(nome){
-        
+        if(nome && nome.trim()!==''){
+            this.#nome = nome;
+            return true;
+        }
+        return false
     }
 
+    getName(){
+        return this.#nome;
+    }
 
+    setEmail(email){
+        if(email && email.trim()!==''){
+            this.#email = email;
+            return true;
+        }
+        return false;
+    }
 
+    getEmail(){
+        return this.#email;
+    }
 }
+
+module.exports = Pessoa;
