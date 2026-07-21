6f64cf36dbf5ef57974e5c8a4e634f04abd8bc99
felipesaliba5-design <felipesaliba5@gmail.com>
2026-07-21 11:07:31 -0300

    implementa classe CarteiraDigital com encapsulamento e operações

--- Exercício de Fixação 03/projeto/carteiraDigital.js
+++ Exercício de Fixação 03/projeto/carteiraDigital.js
@@ -1,1 +1,48 @@
-class CarteiraDigital{}
+
+
+class CarteiraDigital{
+     
+    #titular;
+    #saldo = 0;
+
+    definirTitular(nome){
+        this.#titular = nome;
+    }
+
+    consultarTitular() {
+        let titularAtual = this.#titular;
+        return titularAtual;
+      }
+
+      depositar(valor) {
+        if (valor > 0) {
+          this.#saldo += valor;
+          console.log(`+ Depósito de R$ ${valor.toFixed(2)} realizado com sucesso!`);
+        }
+      }
+
+      sacar(valor){
+        if(valor <= 0){
+          console.log('O valor de saque deve ser maior que zero.');
+          return;
+        }
+        if (valor > this.#saldo) {
+        console.log(`Saldo insuficiente R$ ${valor.toFixed(2)}.`);
+        } 
+        else {
+        this.#saldo -= valor;
+        console.log(`- Saque de R$ ${valor.toFixed(2)}`);
+        }
+      }
+
+      consultarSaldo() {
+        return this.#saldo;
+      }
+
+      exibirInformacoes() {
+        console.log(`Titular: ${this.#titular}`);
+        console.log(`Saldo: R$ ${this.#saldo.toFixed(2)}`);
+      }
+}
+
+module.exports = CarteiraDigital;
